1b3f8ce5732aa29a8f415875f08c4611c24af199
Anurag <anuragdoy123@gmail.com>
2026-07-26 15:53:50 +0545

    updated taylor to fit in verticla

--- shorts/taylor.py
+++ shorts/taylor.py
@@ -10,17 +10,19 @@ config.pixel_height = 1920
 config.frame_width = 9
 config.frame_height = 16
 config.frame_rate = 60
-
 config.background_color = "#0E1117"
 
 class Taylor(Scene):
     def construct(self):
-        head = Text("How close can polynomials get to eˣ?", gradient=(BLUE_C, PURPLE_C)).scale(0.85).to_edge(UP)
+        t1 = Text("How close can polynomials", gradient=(BLUE_C, PURPLE_C)).scale(0.8)
+        t2 = Text("get to eˣ?", gradient=(BLUE_C, PURPLE_C)).scale(0.8)
+        head = VGroup(t1, t2).arrange(DOWN, buff=0.05)
+        head.to_edge(UP)
         self.play(Write(head))
         self.wait(0.7)
 
         formula = MathTex(r"e^x = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \frac{x^4}{4!} + \frac{x^5}{5!} + \cdots", color=GOLD)
-        formula.scale(0.7).next_to(head, DOWN, buff=0.2)
+        formula.scale(0.7).next_to(head, DOWN, buff=0.7)
         self.play(Write(formula))
         self.wait(0.8)
 
@@ -28,12 +30,12 @@ class Taylor(Scene):
         axes.next_to(formula, DOWN, buff=1)
         self.play(Create(axes))
 
-        xlab = MathTex("x").next_to(axes.x_axis.get_end(), RIGHT, buff=0.1)
-        ylab = MathTex("y").next_to(axes.y_axis.get_end(), UP + 0.1 * LEFT, buff=0.1)
+        xlab = MathTex("x").next_to(axes.x_axis.get_end(), RIGHT, buff=0.05)
+        ylab = MathTex("y").next_to(axes.y_axis.get_end(), UP + 0.1 * LEFT, buff=0.05)
         self.play(Write(xlab), Write(ylab))
 
         exp_graph = axes.plot(lambda x: np.exp(x), x_range=[-2, 2], color=GOLD, stroke_width=6)
-        exp_label = MathTex("e^x", color=GOLD).next_to(exp_graph.get_end(), RIGHT + UP, buff=0.1)
+        exp_label = MathTex("e^x", color=GOLD).next_to(exp_graph.get_end(), RIGHT + UP, buff=0.05)
         self.play(Create(exp_graph))
         self.play(Write(exp_label))
 
@@ -46,20 +48,21 @@ class Taylor(Scene):
             MathTex(r"1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \frac{x^4}{4!} + \frac{x^5}{5!}", color=BLUE),
         ]
 
-        scales = [1.0, 0.85, 0.7, 0.55, 0.45, 0.35]
-        
+        scales = [0.7, 0.7, 0.5, 0.5, 0.5, 0.5]
+
         current_graph = axes.plot(lambda x: 1, x_range=[-2, 2], color=BLUE, stroke_width=3)
-        current_label = approximations[0].scale(scales[0]).next_to(current_graph.get_end(), UP, buff=0.1)
+        current_label = approximations[0].scale(scales[0]).next_to(current_graph.get_end(), UP, buff=0.05)
         self.play(Create(current_graph), Write(current_label))
         self.wait(1)
-        
+
+        label_spot = axes.c2p(2, 1.5)
         for i in range(1, 6):
             func = lambda x, n=i: sum([x**j / math.factorial(j) for j in range(n+1)])
             new_graph = axes.plot(func, x_range=[-2, 2], color=BLUE, stroke_width=3)
             new_label = approximations[i].scale(scales[i])
             if i <= 1:
-                new_label.next_to(new_graph.get_end(), UP, buff=0.1)
+                new_label.next_to(new_graph.get_end(), UP, buff=0.05)
             else:
-                new_label.next_to(axes.c2p(2, 4), RIGHT, buff=0.1).shift(LEFT * 0.4)
+                new_label.move_to(label_spot)
             self.play(Transform(current_label, new_label), Transform(current_graph, new_graph))
             self.wait(1)
