0a32714db7b51548e6733415fb8d69e1e67a9f6e
Anurag <anuragdoy123@gmail.com>
2026-08-23 08:54:54 +0545

    completed the scene for flat world

--- long/parallel_lines.py
+++ long/parallel_lines.py
@@ -49,5 +49,48 @@ class ParallelLines(Scene):
 
         distance_marker = VGroup(connector, connector_label)
 
-        self.play(distance_marker.animate.shift(DOWN * 3), runtime = 3)
+        self.play(distance_marker.animate.shift(DOWN * 3), run_time = 2)
+        self.play(distance_marker.animate.shift(UP * 3), run_time = 2)
         self.wait(1)
+
+        ## constant distance indication
+
+        distance_constant = MathTex("d = \\text{constant}")
+        distance_constant.move_to(connector_label)
+
+        self.play(Transform(connector_label,distance_constant), run_time = 1.5)
+        self.wait(1)
+
+        ## fading out everything a part from line
+        
+        self.play(FadeOut(line_ab,label_a,label_b,right_a,right_b,distance_marker))
+        self.wait(1)
+
+        ## constructing the parallel chevron
+
+        P = perpendicular_a.get_center() + UP * 0.3
+
+        shift_val = 0.2
+        p1_line = Line(P, P + DOWN * shift_val + RIGHT * shift_val)
+        p2_line = Line(P, P + DOWN *  shift_val+ LEFT * shift_val)
+
+        parallel_mark_a = VGroup(p1_line, p2_line)
+        self.play(Create(parallel_mark_a))
+
+
+        s = perpendicular_b.get_center() + UP * 0.3 
+        p3_line = Line(s, s + DOWN * shift_val + RIGHT * shift_val)
+        p4_line = Line(s, s + DOWN * shift_val + LEFT * shift_val) 
+
+        parallel_mark_b = VGroup(p3_line, p4_line)
+        self.play(Create(parallel_mark_b))
+        self.wait(1)
+
+        ## removing every remaining object from flat world
+        self.play(FadeOut(perpendicular_a, perpendicular_b, parallel_mark_a, parallel_mark_b), run_time = 1)
+
+        question = Text("But What if space isn't flat?")
+        self.play(Write(question), run_time = 1)
+        self.wait(1.5)
+
+        self.play(FadeOut(question), run_time = 1)
